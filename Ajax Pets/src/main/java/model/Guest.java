e5984e18843e6e891508aa2809b692692347ed86
AndrewThomasW <54901855+AndrewThomasW@users.noreply.github.com>
2020-03-08 08:13:58 -0500

    JavaScript and AJAX
    JavaScript and AJAX

---
+++ Ajax Pets/src/main/java/model/Guest.java
@@ -0,0 +1,20 @@
+package model;
+
+public class Guest {
+    //fields
+    String name;
+    Integer age;
+
+    public Guest(String name,Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Guest{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
